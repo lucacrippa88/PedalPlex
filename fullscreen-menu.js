89ff32cb29c4740b89368a92938ea403abad87a3
Luca Crippa <luca.crippa88@gmail.com>
2025-07-22 18:59:07 +0200

    test redirect

--- fullscreen-menu.js
+++ fullscreen-menu.js
@@ -100,7 +100,7 @@ $(document).ready(function () {
         });
 
         setTimeout(() => {
-          window.location.href = '/';
+          window.location.href = '/PedalPlex/';
         }, 2000);
       }
     });
