f8245ea0a57519a6444ff74a6ec6a42b09d135b7
Luca Crippa <luca.crippa88@gmail.com>
2025-07-23 11:04:43 +0200

    typo fix

--- utils.js
+++ utils.js
@@ -128,7 +128,7 @@ function getPedalsInPreset(songPresetArray) {
       case "wide": return "550px";
       case "wider": return "600px";
       case "widest": return "700px";
-      case "xwidest": return "700px";
+      case "xwidest": return "750px";
       default: return "190px";
     }
   }
