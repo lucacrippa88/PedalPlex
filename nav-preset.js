480673250fcca7fe9390164153318d3571e3e0c2
Luca Crippa <luca.crippa88@gmail.com>
2025-09-15 12:03:32 +0200

    test

--- nav-preset.js
+++ nav-preset.js
@@ -4,10 +4,7 @@ function initNavPreset() {
   <!-- Left: menu toggle + title -->
   <div style="display: flex; align-items: center; gap: 1rem;">
     <button class="menu-toggle" id="menuToggle" aria-label="Open menu" style="background:none; border:none; cursor:pointer;">
-      // <svg class="menu-icon" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-      //   <path d="M4 6h24v2H4zm0 9h24v2H4zm0 9h24v2H4"/>
-      // </svg>
-      <img src='logos/pedalplex_logo_gradient.png' width="25px">
+      <img src='logos/pedalplex_logo_white.png' width="25px">
     </button>
 
     <div class="title">
