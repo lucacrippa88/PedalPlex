b6e258f3be7f2802d6a9c9acf8ae23d7efa0ca55
Luca Crippa <luca.crippa88@gmail.com>
2025-09-16 14:49:53 +0200

    added props css

--- utils.js
+++ utils.js
@@ -997,7 +997,7 @@ function renderPedal(pedal, userRole) {
     const allowedProps = ["color", "font-size", "font-weight", "font-style", "font-family", 
                           "background-color", "padding", "position", "margin", "margin-bottom", "margin-top", 
                           "bottom", "top", "left", "right", "letter-spacing", "word-spacing", "display", 
-                          "line-height", "transform", "height", "width", "border-radius"];
+                          "line-height", "transform", "height", "width", "border-radius", "box-shadow"];
     const safeRules = inputStyle.split(";").filter(rule => {
       const [prop] = rule.split(":");
       return allowedProps.includes(prop.trim().toLowerCase());
