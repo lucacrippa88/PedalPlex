a33f600046997c54bdb385fc66c3eb7492ed6d2d
Luca Crippa <luca.crippa88@gmail.com>
2025-09-16 14:58:38 +0200

    added prop

--- utils.js
+++ utils.js
@@ -995,8 +995,8 @@ function renderPedal(pedal, userRole) {
 
     // Optionally allow only certain props
     const allowedProps = ["color", "font-size", "font-weight", "font-style", "font-family", 
-                          "background-color", "padding", "position", "margin", "margin-left", "margin-right", "margin-bottom", "margin-top", 
-                          "bottom", "top", "left", "right", "letter-spacing", "word-spacing", "display", 
+                          "background-color", "padding", "position", "margin", "margin-left", "margin-right", "margin-bottom", 
+                          "bottom", "top", "left", "right", "letter-spacing", "word-spacing", "display", "border", "margin-top", 
                           "line-height", "transform", "height", "width", "border-radius", "box-shadow", "background-size"];
     const safeRules = inputStyle.split(";").filter(rule => {
       const [prop] = rule.split(":");
