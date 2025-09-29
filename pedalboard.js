8471253d848a43645ab925428aa8ef6c7ef4fd89
Luca Crippa <luca.crippa88@gmail.com>
2025-09-30 00:23:56 +0200

    test

--- pedalboard.js
+++ pedalboard.js
@@ -170,12 +170,16 @@ if (userRole === 'guest') {
   if (!savedBoard) {
     $("#pedalboard-controls").css("display", "none");
     resultsDiv.innerHTML = `
-      <div style="text-align: center; margin-top: 40px;">
-        <p style="font-size: 1.1em; margin-bottom: 20px;">
-          No pedalboard found. Create a new one!
-        </p>
-        <button id="createBtn" class="bx--btn bx--btn--secondary">Create pedalboard</button>
-      </div>`;
+      <button id="createBtn" class="showDesktop bx--btn bx--btn--secondary" type="button" aria-label="Create New Pedalboard"
+                style="display: flex; align-items: center; gap: 0.5rem;">
+                <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16" viewBox="0 0 32 32" aria-hidden="true" class="bx--btn__icon">
+                  <g transform="rotate(90 16 16)">
+                    <path d="M28 24L24 24 24 20 22 20 22 24 18 24 18 26 22 26 22 30 24 30 24 26 28 26 28 24z"></path>
+                    <path d="M10,28V10H22v7h2V6a2.0023,2.0023,0,0,0-2-2H10A2.002,2.002,0,0,0,8,6V28a2.0023,2.0023,0,0,0,2,2h6l0-2ZM10,6H22l0,2H10Z"></path>
+                  </g>
+                </svg>
+                Create pedalboard
+              </button>`;
     window.allPedalboards = [];
     return;
   }
