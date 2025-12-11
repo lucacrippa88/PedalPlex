ee8436dfb25db719d1e3878e22d6f2fc2f2582fe
Luca Crippa <luca.crippa88@gmail.com>
2025-12-12 00:33:17 +0100

    test

--- utils.js
+++ utils.js
@@ -90,9 +90,9 @@ function getRotationFromValue(control, value) {
     case "tenToTwo": angleRange = 120; angleOffset = 300; break;
     case "twelveToSix": angleRange = 180; angleOffset = 0; break;
     case "elevenToSeven": angleRange = -120; angleOffset = 330; break;
-    case "elevenToNineteen": angleRange = -120; angleOffset = 330; break;
+    case "elevenToNineteen": angleRange = -120; angleOffset = 330; break; // sbagliato
     case "oneToFive": angleRange = 120; angleOffset = 30; break;
-    case "oneToTenClock": angleRange = -270; angleOffset = 30; break;
+    case "oneToTenClock": angleRange = 135; angleOffset = 195; break; // testing
     default: angleRange = 270; angleOffset = -135;
   }
 
