691a6fb5101e1bc8318407fdaad095074325f707
Luca Crippa <luca.crippa88@gmail.com>
2025-12-23 16:52:14 +0100

    test

--- utils.js
+++ utils.js
@@ -1811,54 +1811,54 @@ async function renderFullPedalboard() {
 
 
         // ============== AI DROPDOWN PRESET ===============
-        if (window.currentUser && window.currentUser.role === "admin") {
+        // if (window.currentUser && window.currentUser.role === "admin") {
 
-          const $presetContainer = $(`
-            <div class="preset-container">
-              <svg class="preset-icon"
-                focusable="false"
-                preserveAspectRatio="xMidYMid meet"
-                fill="currentColor"
-                width="32"
-                height="32"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg">
-                <path d="M19 21v-2h1v-7h-1v-2h4v2h-1v7h1v2h-4zM15.5005 21h2l-3.5005-11h-3l-3.4966 11h1.9988l.6018-2h4.7781l.6184 2zM10.7058 17l1.6284-5.4111.2559-.0024 1.6736 5.4136h-3.5579z"></path>
-                <path d="M32,32H0V0h32v32ZM2,30h28V2H2v28Z"></path>
-              </svg>
+        //   const $presetContainer = $(`
+        //     <div class="preset-container">
+        //       <svg class="preset-icon"
+        //         focusable="false"
+        //         preserveAspectRatio="xMidYMid meet"
+        //         fill="currentColor"
+        //         width="32"
+        //         height="32"
+        //         viewBox="0 0 32 32"
+        //         xmlns="http://www.w3.org/2000/svg">
+        //         <path d="M19 21v-2h1v-7h-1v-2h4v2h-1v7h1v2h-4zM15.5005 21h2l-3.5005-11h-3l-3.4966 11h1.9988l.6018-2h4.7781l.6184 2zM10.7058 17l1.6284-5.4111.2559-.0024 1.6736 5.4136h-3.5579z"></path>
+        //         <path d="M32,32H0V0h32v32ZM2,30h28V2H2v28Z"></path>
+        //       </svg>
 
-              <div class="preset-dropdown-wrapper">
-                <ul class="preset-dropdown"></ul>
-              </div>
-            </div>
-          `);
+        //       <div class="preset-dropdown-wrapper">
+        //         <ul class="preset-dropdown"></ul>
+        //       </div>
+        //     </div>
+        //   `);
 
-          $wrapper.append($presetContainer);
+        //   $wrapper.append($presetContainer);
 
-          const $dropdownWrapper = $presetContainer.find(".preset-dropdown-wrapper");
-          const $ul = $presetContainer.find(".preset-dropdown");
+        //   const $dropdownWrapper = $presetContainer.find(".preset-dropdown-wrapper");
+        //   const $ul = $presetContainer.find(".preset-dropdown");
 
-          // 🔁 Load presets for this pedal
-          buildPresetDropdown($ul, pedal._id);
+        //   // 🔁 Load presets for this pedal
+        //   buildPresetDropdown($ul, pedal._id);
 
-          // ✨ Glow
-          const glowEl = $dropdownWrapper[0];
-          let stopGlow = null;
-          if (glowEl) stopGlow = startGlow(glowEl);
+        //   // ✨ Glow
+        //   const glowEl = $dropdownWrapper[0];
+        //   let stopGlow = null;
+        //   if (glowEl) stopGlow = startGlow(glowEl);
 
-          // Toggle dropdown
-          $presetContainer.find(".preset-icon").on("click", function (e) {
-            e.stopPropagation();
+        //   // Toggle dropdown
+        //   $presetContainer.find(".preset-icon").on("click", function (e) {
+        //     e.stopPropagation();
 
-            if ($dropdownWrapper.hasClass("is-open")) {
-              $dropdownWrapper.removeClass("is-open");
-            } else {
-              $(".preset-dropdown-wrapper").removeClass("is-open");
-              $dropdownWrapper.addClass("is-open");
-            }
-          });
+        //     if ($dropdownWrapper.hasClass("is-open")) {
+        //       $dropdownWrapper.removeClass("is-open");
+        //     } else {
+        //       $(".preset-dropdown-wrapper").removeClass("is-open");
+        //       $dropdownWrapper.addClass("is-open");
+        //     }
+        //   });
 
-        }
+        // }
         // ================================================
 
 
