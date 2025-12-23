4296dd8c1c1b9fae41e2ac941238553c58949600
Luca Crippa <luca.crippa88@gmail.com>
2025-12-24 00:46:02 +0100

    fix ui

--- utils.js
+++ utils.js
@@ -1910,7 +1910,7 @@ function renderPresetList($ul, pedalId, presets) {
     const $name = $("<span>").addClass("preset-name").text(preset.presetName || preset._id);
 
     // ℹ icon with tooltip event
-    const $info = $("<span>").addClass("preset-info").html('<svg focusable="false" preserveAspectRatio="xMidYMid meet" fill="currentColor" width="12" height="12" viewBox="0 0 32 32" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M17 22L17 14 13 14 13 16 15 16 15 22 12 22 12 24 20 24 20 22 17 22zM16 8a1.5 1.5 0 101.5 1.5A1.5 1.5 0 0016 8z"></path><path d="M16,30A14,14,0,1,1,30,16,14,14,0,0,1,16,30ZM16,4A12,12,0,1,0,28,16,12,12,0,0,0,16,4Z"></path><title>Information</title></svg>');
+    const $info = $("<span>").addClass("preset-info").html(' <svg focusable="false" preserveAspectRatio="xMidYMid meet" fill="currentColor" width="12" height="12" viewBox="0 0 32 32" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M17 22L17 14 13 14 13 16 15 16 15 22 12 22 12 24 20 24 20 22 17 22zM16 8a1.5 1.5 0 101.5 1.5A1.5 1.5 0 0016 8z"></path><path d="M16,30A14,14,0,1,1,30,16,14,14,0,0,1,16,30ZM16,4A12,12,0,1,0,28,16,12,12,0,0,0,16,4Z"></path><title>Information</title></svg>');
     $info.on("mouseenter", (e) => {
       const $tooltip = $(`<div class="preset-tooltip-popup">${description}</div>`);
       $("body").append($tooltip);
