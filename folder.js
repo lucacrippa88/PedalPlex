641e6d7524110022070f97206e6dc1d5168bd6eb
Luca Crippa <luca.crippa88@gmail.com>
2026-03-26 11:48:09 +0100

    fix typo

--- folder.js
+++ folder.js
@@ -206,7 +206,7 @@ function attachAddFolderListener() {
       title: `New Folder for Rig "${boardName}"`,
       input: 'text',
       inputLabel: 'Folder Name',
-      confirmButtonText: "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d='M13 24 4 15 5.414 13.586 13 21.171 26.586 7.586 28 9 13 24z'></path></svg>Create",
+      confirmButtonText: "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d='M13 24 4 15 5.414 13.586 13 21.171 26.586 7.586 28 9 13 24z'></path></svg>Create Folder",
       inputPlaceholder: 'Enter folder name',
       showCancelButton: false,
       showCloseButton: true,
