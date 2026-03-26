8bf8081732913db2ab700ca4f9e99b9729b5fd39
Luca Crippa <luca.crippa88@gmail.com>
2026-03-26 12:36:26 +0100

    test

--- nav-rigs.js
+++ nav-rigs.js
@@ -222,7 +222,8 @@ function initNavPedalboard(userRole) {
             confirmButtonText: "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d='M18 6 16.57 7.393 24.15 15 4 15 4 17 24.15 17 16.57 24.573 18 26 28 16 18 6z'></path></svg>Ok, Next",
             customClass: {
               confirmButton: 'bx--btn bx--btn--primary',
-              cancelButton: 'bx--btn bx--btn--secondary'
+              cancelButton: 'bx--btn bx--btn--secondary',
+              input: 'angleSelect'
             },
             buttonsStyling: false
           });
@@ -242,7 +243,7 @@ function initNavPedalboard(userRole) {
             inputValue: '1',
             // showCancelButton: true,
             showCloseButton: true,
-            confirmButtonText: "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d=‘M26,30H6a2,2,0,0,1-2-2V16a2,2,0,0,1,2-2H9v2H6V28H26V16H23V14h3a2,2,0,0,1,2,2V28A2,2,0,0,1,26,30Z’></path><path d=‘M13 20H19V22H13z’></path><path d=‘M20.59 8.59 17 12.17 17 2 15 2 15 12.17 11.41 8.59 10 10 16 16 22 10 20.59 8.59z’></path></svg>Drop Gear",
+            confirmButtonText: "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d='M26,30H6a2,2,0,0,1-2-2V16a2,2,0,0,1,2-2H9v2H6V28H26V16H23V14h3a2,2,0,0,1,2,2V28A2,2,0,0,1,26,30Z'></path><path d='M13 20H19V22H13z'></path><path d='M20.59 8.59 17 12.17 17 2 15 2 15 12.17 11.41 8.59 10 10 16 16 22 10 20.59 8.59z'></path></svg>Drop Gear",
             customClass: {
               confirmButton: 'bx--btn bx--btn--primary',
               cancelButton: 'bx--btn bx--btn--secondary'
