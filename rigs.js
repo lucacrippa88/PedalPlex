3a779ad0b38ccf758e4abdede9b1aa6d5b504846
Luca Crippa <luca.crippa88@gmail.com>
2026-03-26 12:59:40 +0100

    bugfix

--- rigs.js
+++ rigs.js
@@ -541,7 +541,7 @@ function openEditPedalModal(pbPedal) {
       const removeBtn = document.createElement('button');
       removeBtn.id = 'removePedalBtn';
       removeBtn.className = 'bx--btn bx--btn--danger';
-      removeBtn.textContent = "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d='M16,4c6.6,0,12,5.4,12,12s-5.4,12-12,12S4,22.6,4,16S9.4,4,16,4 M16,2C8.3,2,2,8.3,2,16s6.3,14,14,14s14-6.3,14-14 S23.7,2,16,2z'></path><path d='M8 15H24V17H8z'></path></svg>Remove";
+      removeBtn.innerHTML = "<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d='M16,4c6.6,0,12,5.4,12,12s-5.4,12-12,12S4,22.6,4,16S9.4,4,16,4 M16,2C8.3,2,2,8.3,2,16s6.3,14,14,14s14-6.3,14-14 S23.7,2,16,2z'></path><path d='M8 15H24V17H8z'></path></svg>Remove";
       actions.insertBefore(removeBtn, actions.firstChild);
 
       removeBtn.addEventListener('click', () => {
