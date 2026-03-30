ed687ecfb41c4d31c42d3676d9eba73f55a7d467
Luca Crippa <luca.crippa88@gmail.com>
2026-03-30 11:15:55 +0200

    test btn icon only

--- plexes-share.js
+++ plexes-share.js
@@ -27,10 +27,11 @@ function openShareModal() {
         <div id="shareLinkContainer" style="margin-top:1rem; display:none;">
             <div style="display:flex; gap:8px;">
             <input id="shareLinkInput" class="bx--text-input" readonly style="flex:1;">
-            <button id="copyLinkBtn" class="bx--btn bx--btn--secondary">
-                Copy
+            <button id="copyLinkBtn" class="bx--btn bx--btn--secondary bx--btn--icon-only">
+                <svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'><path d='M6 18H20V20H6z'></path><path d='M6 12H26V14H6z'></path></svg>
             </button>
             </div>
+            <br>
         </div>
 
         </div>
