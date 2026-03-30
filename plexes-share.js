c21977e4e61dd9619a2ee3b24e252c4062bc40a9
Luca Crippa <luca.crippa88@gmail.com>
2026-03-30 14:53:21 +0200

    test auto-update ui

--- plexes-share.js
+++ plexes-share.js
@@ -77,11 +77,21 @@ function openShareModal() {
             const input = document.getElementById("shareLinkInput");
             const copyBtn = document.getElementById("copyLinkBtn");
 
+            // inizializza toggle basandosi sul valore salvato
+            toggle.checked = !!preset.shared;
+
+            // aggiorna UI toggle + link
             function updateUI() {
                 if (toggle.checked) {
                     label.textContent = "Shared";
                     container.style.display = "block";
-                    input.value = generateShareLink(preset);
+
+                    // Popola il link solo se esiste il token
+                    if (preset.shared_token) {
+                        input.value = window.location.origin + '/shared/plex/' + preset.shared_token;
+                    } else {
+                        input.value = '';
+                    }
                 } else {
                     label.textContent = "Private";
                     container.style.display = "none";
@@ -89,20 +99,22 @@ function openShareModal() {
                 }
             }
 
-            // init
+            // chiamata iniziale per aggiornare l'UI
             updateUI();
 
+            // cambia UI al toggle
             toggle.addEventListener("change", updateUI);
 
+            // copy button
             copyBtn.addEventListener("click", async () => {
                 try {
                     await navigator.clipboard.writeText(input.value);
                     copyBtn.textContent = "Copied!";
                     setTimeout(() => {
                         copyBtn.innerHTML = `<svg focusable='false' preserveAspectRatio='xMidYMid meet' xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='16' height='16' viewBox='0 0 32 32' aria-hidden='true' class='bx--btn__icon'>
-              <path d='M29.25,6.76a6,6,0,0,0-8.5,0l1.42,1.42a4,4,0,1,1,5.67,5.67l-8,8a4,4,0,1,1-5.67-5.66l1.41-1.42-1.41-1.42-1.42,1.42a6,6,0,0,0,0,8.5A6,6,0,0,0,17,25a6,6,0,0,0,4.27-1.76l8-8A6,6,0,0,0,29.25,6.76Z'></path>
-              <path d='M4.19,24.82a4,4,0,0,1,0-5.67l8-8a4,4,0,0,1,5.67,0A3.94,3.94,0,0,1,19,14a4,4,0,0,1-1.17,2.85L15.71,19l1.42,1.42,2.12-2.12a6,6,0,0,0-8.51-8.51l-8,8a6,6,0,0,0,0,8.51A6,6,0,0,0,7,28a6.07,6.07,0,0,0,4.28-1.76L9.86,24.82A4,4,0,0,1,4.19,24.82Z'></path>
-            </svg>`;
+                            <path d='M29.25,6.76a6,6,0,0,0-8.5,0l1.42,1.42a4,4,0,1,1,5.67,5.67l-8,8a4,4,0,1,1-5.67-5.66l1.41-1.42-1.41-1.42-1.42,1.42a6,6,0,0,0,0,8.5A6,6,0,0,0,17,25a6,6,0,0,0,4.27-1.76l8-8A6,6,0,0,0,29.25,6.76Z'></path>
+                            <path d='M4.19,24.82a4,4,0,0,1,0-5.67l8-8a4,4,0,0,1,5.67,0A3.94,3.94,0,0,1,19,14a4,4,0,0,1-1.17,2.85L15.71,19l1.42,1.42,2.12-2.12a6,6,0,0,0-8.51-8.51l-8,8a6,6,0,0,0,0,8.51A6,6,0,0,0,7,28a6.07,6.07,0,0,0,4.28-1.76L9.86,24.82A4,4,0,0,1,4.19,24.82Z'></path>
+                        </svg>`;
                     }, 1500);
                 } catch (e) {
                     console.error("Copy failed", e);
