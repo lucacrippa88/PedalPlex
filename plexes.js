84002b2327d3314aaf8247fc64bdf4e856c34992
Luca Crippa <luca.crippa88@gmail.com>
2026-04-03 22:42:34 +0200

    bugfix

--- plexes.js
+++ plexes.js
@@ -6,8 +6,8 @@ window.isPreviewMode = !!sharedToken;
 function setupPreviewMode() {
   if (!window.isPreviewMode) return;
 
-  $('#preset').css('display','none');
-  $('#previewRig').css('display','block');
+  // $('#preset').css('display','none');
+  // $('#previewRig').css('display','block');
   
   // Assicurati che gli elementi esistano
   const ids = [
